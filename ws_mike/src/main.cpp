49f2b2d98a501d9b05d3cb351d17f22827df23c1
Miguel Riem <mriem@ua.pt>
2013-07-03 15:44:37 +0100

    sad

--- ws_mike/src/main.cpp
+++ ws_mike/src/main.cpp
@@ -7,12 +7,15 @@
 //Global variable dsfsdf
 std::string _name="mike";
 ros::Publisher chatter_pub;
+double _pos_x;
+double _pos_y;
 
 //void chatterCallback(const std_msgs::String::ConstPtr& msg_in)
 void chatterCallback(const ws_referee::custom::ConstPtr& msg_in)
 {
 	//ROS_INFO("%s: heard: [%s]", _name.c_str(), msg_in->data.c_str());
-	ROS_INFO("%s: Received msg",_name.c_str());
+	ROS_INFO("%s: Received msg with dist=%f",_name.c_str(), msg_in->dist);
+	_pos_x += _pos_x;
 
 	//Publish new msg
 	//std_msgs::String msg_out;
@@ -23,7 +26,7 @@ void chatterCallback(const ws_referee::custom::ConstPtr& msg_in)
 	msg_out.sender = _name;
 	msg_out.winner = "";
 	chatter_pub.publish(msg_out);
-	ROS_INFO("%s: will publish a message",_name.c_str());
+	ROS_INFO("%s: I am at %f.  will publish a message",_name.c_str(), _pos_x);
 }
 
 int main(int argc, char **argv)
@@ -32,6 +35,9 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 	//chatter_pub = n.advertise<std_msgs::String>("player_out", 1);
 	chatter_pub = n.advertise<ws_referee::custom>("player_out", 1);
+
+	_pos_x = 0;
+	_pos_y = 1; //to be read from a param
 
 	ros::Subscriber sub = n.subscribe("player_in", 1, chatterCallback);
 	ros::Rate loop_rate(2);
