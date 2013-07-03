062bbe91344974de81177d4e5b22f68316333077
Miguel Riem <mriem@ua.pt>
2013-07-03 17:15:46 +0100

    mike

--- ws_mike/src/main.cpp
+++ ws_mike/src/main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 
 #include <ws_referee/custom.h>
+#include <ws_referee/randomize.h>
 #include <visualization_msgs/Marker.h>
 
 //Global variable 
@@ -14,14 +15,17 @@ double _pos_y;
 
 void chatterCallback(const ws_referee::custom::ConstPtr& msg_in)
 {
+	
 	ROS_INFO("%s: Received msg with dist=%f",_name.c_str(), msg_in->dist);
 	_pos_x += msg_in->dist;
 
+	//check if you won
+	
 	//Publish new msg
 	ws_referee::custom msg_out;
 
 
-	msg_out.dist = 0.5;
+	msg_out.dist = get_random_num(0, 0.1);
 	msg_out.sender = _name;
 	msg_out.winner = "";
 	chatter_pub.publish(msg_out);
@@ -30,32 +34,35 @@ void chatterCallback(const ws_referee::custom::ConstPtr& msg_in)
 	visualization_msgs::Marker marker;
 	marker.header.frame_id = "world";
 	marker.header.stamp = ros::Time();
-	marker.ns = "";
-	marker.id = 0;
+	marker.ns = ""; marker.id = 0;
 	marker.type = visualization_msgs::Marker::SPHERE;
 	marker.action = visualization_msgs::Marker::ADD;
-	marker.pose.position.x = _pos_x;
-	marker.pose.position.y = _pos_y;
-	marker.pose.position.z = 0;
-	marker.pose.orientation.x = 0.0;
-	marker.pose.orientation.y = 0.0;
-	marker.pose.orientation.z = 0.0;
-	marker.pose.orientation.w = 1.0;
-	marker.scale.x = 0.3;
-	marker.scale.y = 0.3;
-	marker.scale.z = 0.3;
-	marker.color.a = 1.0;
-	marker.color.r = 1.0;
-	marker.color.g = 0.0;
+	marker.pose.position.x = _pos_x; marker.pose.position.y = _pos_y;
+	marker.pose.position.z = 0; marker.pose.orientation.x = 0.0; marker.pose.orientation.y = 0.0;
+	marker.pose.orientation.z = 0.0; marker.pose.orientation.w = 1.0;
+	marker.scale.x = 0.3; marker.scale.y = 0.3;	marker.scale.z = 0.3;
+	marker.color.a = 0.1; marker.color.r = 1.0; marker.color.g = 0.0;
 	marker.color.b = 0.0;
 	marker_pub.publish( marker );
-
+	
+	//a new text marker
+	marker.id = 1;
+	marker.color.r = 0.4;
+	marker.pose.position.x = _pos_x - 0.3;
+	marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+	marker.text = "mike";
+	marker_pub.publish( marker );
 }
 
 int main(int argc, char **argv)
 {
+	
 	ros::init(argc, argv, _name);
 	ros::NodeHandle n;
+
+	//init the randomizer
+	init_randomization_seed();
+
 	chatter_pub = n.advertise<ws_referee::custom>("player_out", 1);
 	marker_pub = n.advertise<visualization_msgs::Marker>("mike_marker", 1);
 
